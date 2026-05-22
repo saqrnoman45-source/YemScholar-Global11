737268d49448df81945f60f9f1fa42efa9025cb4
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-22 17:41:44 +0300

    Add Home component with styled div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,17 @@
-
+export default function Home() {
+  return (
+    <div
+      style={{
+        background: "#020617",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px"
+      }}
+    >
+      Home Page 🚀
+    </div>
+  );
+}
