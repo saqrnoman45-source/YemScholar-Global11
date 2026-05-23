b24b6ced2624c6a15752254f1b8d3705bc8305ca
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-23 23:27:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-
 import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import Courses from "./pages/Courses";
 import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
