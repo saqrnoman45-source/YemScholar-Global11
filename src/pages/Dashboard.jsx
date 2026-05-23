0e68c99f52db348df705e7accc1fe596fddb406d
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-23 23:17:28 +0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
 export default function Dashboard() {
   return (
     <div>
-      Dashboard Page
+      <h1>📊 Dashboard</h1>
+
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gap: "15px",
+        marginTop: "20px"
+      }}>
+        <div style={{ background: "#1f2937", padding: "20px", borderRadius: "10px" }}>
+          📚 Courses
+        </div>
+
+        <div style={{ background: "#1f2937", padding: "20px", borderRadius: "10px" }}>
+          👥 Users
+        </div>
+
+        <div style={{ background: "#1f2937", padding: "20px", borderRadius: "10px" }}>
+          ⚡ Activity
+        </div>
+      </div>
     </div>
   );
 }
