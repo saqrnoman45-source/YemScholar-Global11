8d0237a9164e9310bdbfe8a7ea2cab7973e92ca5
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-23 23:23:21 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [name, setName] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    login(name);
+    navigate("/dashboard");
+  };
+
   return (
-    <div>
-      Login Page
+    <div style={{ color: "white", padding: "40px" }}>
+      <h1>🔐 تسجيل الدخول</h1>
+
+      <input
+        placeholder="اسم المستخدم"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ padding: "10px", marginTop: "10px" }}
+      />
+
+      <br />
+
+      <button
+        onClick={handleLogin}
+        style={{ marginTop: "10px", padding: "10px" }}
+      >
+        دخول
+      </button>
     </div>
   );
 }
