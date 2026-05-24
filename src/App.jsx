702f2743447f57967b83fe74cd338c5405ea7c5d
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-24 20:41:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Courses from "./pages/Courses";
