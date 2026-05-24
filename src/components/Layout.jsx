196640db61e21c7aca9dacad4041586e002f26bb
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-24 21:58:47 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,28 @@
 import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div style={{
-      display: "flex",
-      minHeight: "100vh",
-      background: "#0f172a",
-      color: "white"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        background: "#0f172a",
+        color: "white"
+      }}
+    >
       <Sidebar />
 
-      <div style={{ flex: 1 }}>
-        <Navbar />
-        <div style={{ padding: "20px" }}>
-          {children}
-        </div>
+      <div
+        style={{
+          flex: 1,
+          marginRight:
+            window.innerWidth < 768
+              ? "80px"
+              : "256px",
+          padding: "20px"
+        }}
+      >
+        {children}
       </div>
     </div>
   );
