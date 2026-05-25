3f6dd404d96ae1bdcb0cd394846079490b3396d8
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-25 19:20:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Sidebar from "./components/Sidebar";
+
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Courses from "./pages/Courses";
-import CourseDetails from "./pages/CourseDetails";
-import Lesson from "./pages/Lesson";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import CourseDetails from "./pages/CourseDetails";
+import Lesson from "./pages/Lesson";
 
 export default function App() {
   return (
@@ -17,11 +17,20 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/courses" element={<Courses />} />
-        <Route path="/course/:id" element={<CourseDetails />} />
-        <Route path="/course/:courseId/:lessonId" element={<Lesson />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
+
+        {/* Courses */}
+        <Route
+          path="/course/:id"
+          element={<CourseDetails />}
+        />
+
+        <Route
+          path="/course/:courseId/:lessonId"
+          element={<Lesson />}
+        />
       </Routes>
     </Layout>
   );
